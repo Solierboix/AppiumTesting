002e3bf73ee4d7cb18f2dd1304a56d2e5aa80c8a
Solierboix <solierboix1991@gmail.com>
2018-01-24 00:48:27 +0100

    Initial commit with layout for appium Framework

---
+++ src/main/java/Runner.java
@@ -0,0 +1,15 @@
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Runner {
+
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/main/java/core/constans/Credentials.java
@@ -0,0 +1,4 @@
+package core.constans;
+
+public class Credentials {
+}
