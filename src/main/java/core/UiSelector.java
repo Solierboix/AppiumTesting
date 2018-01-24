cb1920fb5835aaa84bd248234465ddd7e84e1deb
Piotr Walczak <piotr.walczak-ext@continental-corporation.com>
2018-01-24 12:05:39 +0100

    Add additional methods to UiSelector

--- src/main/java/core/UiSelector.java
+++ src/main/java/core/UiSelector.java
@@ -8,4 +8,64 @@ public class UiSelector {
         locator += ".resourceId(\""+value+"\")";
         return this;
     }
+
+    public UiSelector className (String value) {
+        locator += ".className(\""+value+"\")";
+        return this;
+    }
+
+    public UiSelector classNameMatches (String regex) {
+        locator += ".classNameMatches(\""+regex+"\")";
+        return this;
+    }
+
+    public UiSelector text (String value) {
+        locator += ".text(\""+value+"\")";
+        return this;
+    }
+
+    public UiSelector textContains (String value) {
+        locator += ".textContains(\""+value+"\")";
+        return this;
+    }
+
+    public UiSelector index (int value) {
+        locator += ".resourceId("+value+")";
+        return this;
+    }
+
+    public UiSelector clickable (boolean value) {
+        locator += ".clickable("+value+")";
+        return this;
+    }
+
+    public UiSelector checked (boolean value) {
+        locator += ".checked("+value+")";
+        return this;
+    }
+
+    public UiSelector enabled (boolean value) {
+        locator += ".enabled("+value+")";
+        return this;
+    }
+
+    public UiSelector description (String value) {
+        locator += ".description(\""+value+"\")";
+        return this;
+    }
+
+    public UiSelector descriptionContains (String value) {
+        locator += ".descriptionContains(\""+value+"\")";
+        return this;
+    }
+
+    public UiSelector descriptionMatches (String value) {
+        locator += ".descriptionMatches(\""+value+"\")";
+        return this;
+    }
+
+    public UiSelector xPath (String xPath) {
+        locator = xPath;
+        return this;
+    }
 }
