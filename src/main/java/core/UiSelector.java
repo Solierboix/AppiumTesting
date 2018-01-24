d720edec868ec407a472065ece17f4e6a3fae2a9
Solierboix <solierboix1991@gmail.com>
2018-01-24 01:09:41 +0100

    First method for UiSelector

--- src/main/java/core/UiSelector.java
+++ src/main/java/core/UiSelector.java
@@ -1,4 +1,11 @@
 package core;
 
 public class UiSelector {
+
+    private String locator = "new UiSelector()";
+
+    public UiSelector resourceId (String value) {
+        locator += ".resourceId(\""+value+"\")";
+        return this;
+    }
 }
